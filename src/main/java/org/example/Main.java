cfe916b094a74b100525dd11ead40537106285f4
batuhan-arslan <batuhanarslangithub@gmail.com>
2025-04-09 21:25:50 +0300

    1.soru tamamlandı.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,26 +2,35 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello World");
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
-    }
 
+        if (clock < 0 || clock > 23) {
+            return false;
+        }
+
+        if (isBarking && (clock < 8 || clock > 20)) {
+            return true;
+
+        }
+        return false;
+    }
+ /*
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+
     }
 
     public static double area(double width, double height) {
-       
+
     }
 
     public static double area(double radius) {
-       
-    }
+
+    } */
 }
