a2b4799443ea59b9e9d98c64f88768b9df2bc9b4
batuhan-arslan <batuhanarslangithub@gmail.com>
2025-04-17 23:02:18 +0300

    1.soru tamamlandı.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,30 +7,43 @@ public class Main {
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
 
-        if (clock < 0 || clock > 23) {
+        if (clock < 0 || clock > 23 || !isBarking) {
             return false;
         }
 
-        if (isBarking && (clock < 8 || clock > 20)) {
-            return true;
-
-        }
-        return false;
+        return clock < 8 || clock >= 20;
     }
- /*
+
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
+        return (firstAge > 13 && firstAge <= 19) || (secondAge > 13 && secondAge <= 19) || (thirdAge > 13 && thirdAge <= 19);
 
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
+        int lowestTemp = 25;
+        int highestTemp = isSummer ? 45 : 35 ;
+
+        return temp < highestTemp && temp > lowestTemp;
 
     }
 
     public static double area(double width, double height) {
+        if ( width < 0 || height < 0) {
+            System.out.println("Width veya height değeri sıfırdan küçük olamaz ! Width: " + width + "Height: " + height);
+            return -1;
+    }
+            return  width * height;
+    }
+
+
+    public static double area(double radius) {
+            if ( radius <0) {
+                System.out.println("Radius sıfırdan küçük olamaz Radius: " + radius);
+                return -1;
+
+            }
+            return Math.PI *Math.pow(radius, 2);
+        }
 
     }
 
-    public static double area(double radius) {
-
-    } */
-}
